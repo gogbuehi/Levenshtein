a873284f06ede3e6f680ce62d60839e44f03b9b2
Goodwin Ogbuehi <github@gogbuehi.com>
2011-12-13 12:52:00 -0800

    Just testing a commit

--- src/levenshtein/Levenshtein.java
+++ src/levenshtein/Levenshtein.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.logging.Logger; //For logging
 
 /**
  *
